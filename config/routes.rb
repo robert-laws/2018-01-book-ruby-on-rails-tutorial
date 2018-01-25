22e6c0542ad9dd788beec03dbc96b6726ec2c274
Robert Laws <roblaws@me.com>
2018-01-25 15:41:00 +0300

    add micropost model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'microposts/index'
+
+  get 'microposts/show'
+
+  get 'microposts/new'
+
+  get 'microposts/create'
+
+  get 'microposts/edit'
+
+  get 'microposts/update'
+
+  get 'microposts/destroy'
+
   # resources :users, only: [:index, :show]
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show', as: 'user'
---
+++ db/migrate/20180125123804_create_microposts.rb
@@ -0,0 +1,10 @@
+class CreateMicroposts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :microposts do |t|
+      t.text :content
+      t.belongs_to :user
+
+      t.timestamps
+    end
+  end
+end
