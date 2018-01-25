95af2a00b4480f72a310bc17f9b477a9d1b959a3
Robert Laws <roblaws@me.com>
2018-01-25 15:22:55 +0300

    setup user model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :users, only: [:index, :show]
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show', as: 'user'
+
+  root 'users#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+User.create(name: "Bob Cobb", email: "cobb@mail.net")
+User.create(name: "Hal Hope", email: "hope@mail.net")
+User.create(name: "Kal Kool", email: "kool@mail.net")
+User.create(name: "Jim Jupe", email: "jupe@mail.net")
