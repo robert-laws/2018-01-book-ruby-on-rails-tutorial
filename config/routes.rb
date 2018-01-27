00bb7c2416c2cc54f08abcc3e0ebf3d1cd0cc059
Robert Laws <roblaws@me.com>
2018-01-27 20:08:16 +0300

    begin basic tests

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
 
   get '/hello', to: 'application#hello'
 
-  # resources :users, only: [:index, :show]
-  get '/doctors', to: 'doctors#index'
-  get '/doctors/:id', to: 'doctors#show', as: 'doctor'
+  get 'doctors/index'
 
-  root 'doctors#index'
+  # get '/doctors', to: 'doctors#index'
+  # get '/doctors/:id', to: 'doctors#show', as: 'doctor'
+
+  # root 'doctors#index'
 end
